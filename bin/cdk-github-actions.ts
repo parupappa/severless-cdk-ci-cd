6b9a2b7d8c42c7452a928e718aecb0806448304c
annosuke.yokoo <annosuke.yokoo@lt-s.jp>
2023-04-05 12:30:23 +0900

    Fixed @aws-cdk/core

--- bin/cdk-github-actions.ts
+++ bin/cdk-github-actions.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { CdkGithubActionsStack } from '../lib/cdk-github-actions-stack';
 
 const app = new cdk.App();
