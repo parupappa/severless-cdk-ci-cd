e8cb23ddba2c62de2857f6fe5ceaff1bdbf07b7c
annosuke.yokoo <annosuke.yokoo@lt-s.jp>
2023-04-05 11:52:56 +0900

    first commit

--- lib/cdk-github-actions-stack.ts
+++ lib/cdk-github-actions-stack.ts
@@ -1,16 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 export class CdkGithubActionsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    
+    // Create a Lambda function with code from the "src/lambda" directory
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('./src/lambda'),
+      handler: 'hello.handler',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkGithubActionsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigateway.LambdaRestApi(this, 'GithubActionsEndpoint', {
+      handler: hello,
+      endpointTypes: [ apigateway.EndpointType.EDGE ],
+    });
   }
 }
